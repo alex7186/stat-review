9d6f738b89d340bd7a3f7dc9c05c815823840e83
alex7186 <sasha2649058@mail.ru>
2021-04-23 16:59:33 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,115 @@
+from flask import Flask, render_template, request, flash, redirect, url_for, send_from_directory
+from datetime import date, datetime
+
+from docx.shared import Inches, Pt
+from docx import Document
+
+import io, os.path, base64
+
+import input_file
+import functions
+
+# Чертовски красивый снег
+# Из окна идёт снег. Скоро все деревья им будут покрыты, и будет очень красиво.
+
+app = Flask(__name__)
+app.config['SECRET KEY'] = input_file.app_secret_key
+app.secret_key = input_file.app_secret_key
+app.config['UPLOAD_FOLDER'] = './uploads'
+
+
+app.config['SESSION_TYPE'] = input_file.app_sessin_type
+app.debug = input_file.app_debug
+
+buf, document, intervals, absolute_frequency, result_file_name, a, k = None, None, None, None, None, None, None
+
+
+variant_and_distribution = 0
+
+@app.route('/', methods=('GET', 'POST'))
+def index():
+
+    global variant_and_distribution
+    global buf, document, intervals, absolute_frequency, result_file_name, a, k
+    content_list = [input_file.app_name, {}]
+        
+
+    # считываем вариант
+    if variant_and_distribution == 0:
+        variant = -1
+        # вариант введен
+        try:
+            variant = int(request.form['variant'])
+            variant_and_distribution = 0.5
+
+        except Exception as e:
+            variant_and_distribution = 0
+            if variant != -1:
+                flash(u'Что-то не так с номером варианта!', 'error')
+
+        # вариант не введен, возвращаемся ко вводу
+        if variant_and_distribution == 0:
+            this_content = content_list
+            this_content[1].update(input_file.variant_selection)
+            return render_template('index.html', content_list=this_content)
+
+        # вариант неверен, выводим сообщение
+        if not variant in functions.available_vars:
+            flash(u'Недопустимый номер варианта', 'error')
+
+
+        # вариант верен, выводим распределение
+        else:
+            result_file_name, a, k = functions.get_variant(variant)
+            document = Document()
+
+            buf, document, intervals, absolute_frequency = functions.pre_return(result_file_name, a, k, document, input_file.color, )
+            data = base64.b64encode(buf.getbuffer()).decode("ascii")
+
+
+            this_content = content_list
+            this_content[1].update(input_file.prepare_dist_selection(data))
+            variant_and_distribution = 2
+            return render_template('index.html', content_list=this_content)
+
+    # страница скачивания и возврата к выбору варианта
+    if variant_and_distribution <=2:
+        try:
+            distr_type = int(request.form['distr_type'])
+        except:
+            pass
+
+        if distr_type:
+            document.add_picture(buf, width=Inches(5.5))
+            document.add_page_break()
+            document = functions.post_return(document, result_file_name, distr_type, a, intervals, k, absolute_frequency)
+
+            buf_document = io.BytesIO()
+            document.save(buf_document)
+
+            # сохраняем готовый файл
+            with open('uploads/'+result_file_name, 'wb') as f:
+                f.write(buf_document.getvalue())
+
+            this_content = content_list
+
+            this_content[1].update(input_file.prepare_download_link(a='/uploads/'+result_file_name, target=result_file_name))
+            variant_and_distribution = 0
+            return render_template('index.html', content_list=this_content)
+
+
+    variant = -1
+    variant_and_distribution = 0
+    this_content = content_list
+    this_content[1].update(input_file.variant_selection)
+    return render_template('index.html', content_list=this_content)
+
+
+@app.route('/uploads/<path:filename>', methods=['GET', 'POST'])
+def download(filename):
+
+    uploads = os.path.join(app.root_path, app.config['UPLOAD_FOLDER'])
+
+    return send_from_directory(directory=uploads, filename=result_file_name)
+
+app.run(host='0.0.0.0', debug=app.debug)
---
+++ input_file.py
@@ -0,0 +1,68 @@
+from flask import render_template, request, flash, redirect, url_for
+# from datetime import date, datetime
+from docx import Document
+
+import matplotlib.pyplot as plt
+from docx.shared import Inches
+
+import functions
+
+# color = 'blue' # #0275d8
+color = (2/255, 117/255, 216/255)
+
+
+app_secret_key = 'my secret key'
+app_sessin_type = 'filesystem'
+app_debug = True
+
+
+app_name = 'Первичная статистическая обработка выборки'
+
+
+content_list = [app_name, {}]
+
+variant_selection = {
+    'post0003': {
+        'type':'form',
+        'title':'Номер варианта',
+        'content':[
+            {'type':'input', 'input_type':'text', 'name':'variant', 'placeholder':'Введите вариат работы от 1 до 76'},
+            # {'type':'input', 'input_type':'text', 'name':'content', 'placeholder':'Post content'},
+            {'type':'button', 'btntype':'submit', 'name':'Продолжить'}
+            ]
+        },
+}
+
+dist_selection = {
+        'post0005': {
+            'type':'form',
+            'title':'Тип распределения:',
+            'content':[
+                # {'type':'output', 'input_type':'show-text', 'placeholder':'Введите тип распределения:'},
+                {'type':'output', 'input_type':'show-image', 'src' : 000},
+                {'type':'input', 'input_type':'radio', 'name':'distr_type', 'id':'0', 'value':'Показательное'},
+                {'type':'input', 'input_type':'radio', 'name':'distr_type', 'id':'1', 'value':'Равномерное'},
+                {'type':'input', 'input_type':'radio', 'name':'distr_type', 'id':'2', 'value':'Нормальное'},
+                {'type':'button', 'btntype':'submit', 'name':'Продолжить'}
+                ]
+            },
+    }
+
+download_link = {
+    'post0006':{
+        'type' : 'form',
+        'title' : 'Ваш отчет готов ✅',
+        'content':[
+            {'type':'output', 'input_type':'show-a', 'a':000, 'target':000},
+            {'type':'button', 'btntype':'submit', 'name':'Вернуться к выбору варианта'}
+        ]
+    }
+}
+
+def prepare_dist_selection(src):
+    dist_selection['post0005']['content'][0].update({'src':'data:image/png;base64,'+str(src)})
+    return dist_selection
+
+def prepare_download_link(a, target):
+    download_link['post0006']['content'][0].update({'a':str(a), 'target':str(target)})
+    return download_link
